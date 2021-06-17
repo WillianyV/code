d9f5ca6428d13f07c6293a9bdc970dfd68ba24eb
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-06-17 17:53:19 -0300

    Seção 06
    vídeo 37

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,25 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*
-Route::get('/', function () {
-    return 'olá, seja bem vindo!';
-});
-*/
 Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
 
 Route::get('/sobrenos',[\App\Http\Controllers\SobrenosController::class,'exibirMsg']);
 
 Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'exibirMsg']);
+
+Route::prefix('/app')->group(function(){
+    Route::get('/login',function(){ return 'LOGIN';});
+    Route::get('/clientes',function(){ return 'CLIENTES';});
+    Route::get('/fornecedores',function(){ return 'FORNECEDORES';});
+    Route::get('/produtos',function(){ return 'PRODUTOS';});
+});
+
+
 
