dd199bb63e3e07c9e2c389a302761d682739cda6
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-06-18 09:35:17 -0300

    finalizando sessão 6
    Rotas avançadas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,18 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
-
-Route::get('/sobrenos',[\App\Http\Controllers\SobrenosController::class,'exibirMsg']);
-
-Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'exibirMsg']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobrenos',[\App\Http\Controllers\SobrenosController::class,'exibirMsg'])->name('site.sobrenos');
+Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'exibirMsg'])->name('site.contato');
 
 Route::prefix('/app')->group(function(){
-    Route::get('/login',function(){ return 'LOGIN';});
-    Route::get('/clientes',function(){ return 'CLIENTES';});
-    Route::get('/fornecedores',function(){ return 'FORNECEDORES';});
-    Route::get('/produtos',function(){ return 'PRODUTOS';});
+    Route::get('/login',function(){ return 'LOGIN';})->name('app.login');
+    Route::get('/clientes',function(){ return 'CLIENTES';})->name('app.clientes');
+    Route::get('/fornecedores',function(){ return 'FORNECEDORES';})->name('app.fornecedores');
+    Route::get('/produtos',function(){ return 'PRODUTOS';})->name('app.protudos');
 });
+/* */
+//redirecinamento de rotas
+Route::get('/rota1', function(){echo 'rota 1';})->name('site.rota1');
+//mais usual
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+//não é usual:
+//Route::redirect('/rota2','rota1');
 
-
-
+Route::fallback(function(){
+    echo 'A página acessada não existe, <a href="'.route('site.index').'">Clique aqui</a> para voltar a pagina inicial.';
+});
