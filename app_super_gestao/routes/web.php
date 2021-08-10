0a4f955e86e5d25bef6aaa65b679107e1117de26
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-08-10 17:28:38 -0300

    aula 174  min 2:30

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -43,6 +43,8 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(
     
     //produtos
     Route::resource('produto', 'ProdutoController');
+
+    Route::resource('produto-detalhe', 'ProdutoDetalheController');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
